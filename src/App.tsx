4573a191487049109a0a7acc699f1068a63effa6
ljq0226 <1035821043@qq.com>
2023-06-25 10:30:57 +0800

    3.添加拖拽

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { useDraggable } from '@neodrag/react'
+import type { DragOptions } from '@neodrag/react'
+import { useRef, useState } from 'react'
 import { BottomBar, TrafficLight } from './components'
 
 function App() {
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const draggableRef = useRef(null)
+
+  // 初始化 dragable 拖拽设置
+  const options: DragOptions = {
+    position,
+    onDrag: ({ offsetX, offsetY }) => setPosition({ x: offsetX, y: offsetY }),
+    bounds: { bottom: -500, top: 32, left: -600, right: -600 },
+    handle: '.window-header',
+    cancel: '.traffic-lights',
+  }
+  useDraggable(draggableRef, options)
+
   return (
-    <div className='relative w-[700px] h-[500px] flex flex-col' >
+    <div ref={draggableRef} className='relative w-[700px] h-[500px] flex flex-col' >
     <div className='absolute w-full top-2 window-header'>
       <TrafficLight />
     </div>
